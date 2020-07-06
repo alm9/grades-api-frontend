ad35e52dd098f3e65852d622791141069f9fe716
andre.machado <4ndre.machado@gmail.com>
2020-07-05 21:30:52 -0300

    add procfile to heroku

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:3001/',
+  // baseURL: 'http://localhost:3001/',
+  baseURL: 'https://sleepy-meadow-36095.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
   },

264c9884ff11ddb21e0681254d9b643c11974b8a
andre.machado <4ndre.machado@gmail.com>
2020-07-05 23:14:25 -0300

    changed react-scripts version from 3.4.1. to 3.4.0

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  // baseURL: 'http://localhost:3001/',
+  // baseURL: 'http://localhost:3001/', //localmente
   baseURL: 'https://sleepy-meadow-36095.herokuapp.com/',
   headers: {
     'Content-type': 'application/json',
